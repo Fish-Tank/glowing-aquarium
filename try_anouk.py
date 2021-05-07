3591c76d76773df5b62b96a4a579a9221cc337a8
NUK95 <83701192+NUK95@users.noreply.github.com>
2021-05-07 14:31:02 +0200

    Update try_anouk.py

--- try_anouk.py
+++ try_anouk.py
@@ -1,11 +1,11 @@
 import pandas as pd
 account_data = ad = pd.read_csv("accounts.txt", delimiter=" ")
-#fish_data = fd = pd.read_csv("fish.txt", delimiter=" ")
+fish_data = fd = pd.read_csv("fish.txt", delimiter=" ")
 
 #for index, row in accounts.iterrows():
 #    print(index, row['username'])
 print(ad.columns)
-#print(fd.columns)
+print(fd.columns)
 
 #fd.loc[fd["password"] == "hallo"]
 
