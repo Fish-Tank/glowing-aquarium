a1abeae4944c0596a0b5401ae3a262872297dc77
NUK95 <83701192+NUK95@users.noreply.github.com>
2021-05-07 10:38:47 +0200

    changes

--- Read the Fish.py
+++ Read the Fish.py
@@ -1,43 +1,81 @@
 with open ("accounts.txt", "r") as accounts:
-    lines = accounts.readlines()
-    print(lines)
+    a_lines = accounts.readlines()
+    print(a_lines)
 
 user_names = []
 email = []
-password = []
-for l in lines:
+a_password = []
+for l in a_lines:
     as_list = l.split(" ")
     user_names.append(as_list[0])
     email.append(as_list[1])
-    password.append(as_list[2])
+    a_password.append(as_list[2])
 
 print(user_names)
 print(email)
-print(password)
+print(a_password)
 
-accounts.close()
 
-with open ("fish.txt", "r") as fish:
-    lines = fish.readlines()
-    print(lines)
+with open("fish.txt", "r") as fish:
+    f_lines = fish.readlines()
+    print(f_lines)
+
 
 fish_type = []
 length = []
 lake = []
-password = []
-for l in lines:
+f_password = []
+for l in f_lines:
     as_list = l.split(" ")
     fish_type.append(as_list[0])
     length.append(as_list[1])
     lake.append(as_list[2])
-    password.append(as_list[3])
+    f_password.append(as_list[3])
 
 print(fish_type)
 print(length)
 print(lake)
-print(password)
+print(f_password)
 
-fish.close()
+commands = [
+            "exit > exit the program",
+            "help > show all the commands",
+            "profile > show all my profile details ",
+            "achievements > show me my cought fish",
+            "rating list > enter a fish",
+        ]
+
+current_user = {
+    "name": None,
+    "password": None,
+}
+
+while True:
+    if current_user["name"] == None:
+        user_input = input("Enter a command or type help :>> ").lower()
+    else:
+        user_input = input(
+            "(Welcome: %s) Enter a command or type help :>> " % current_user["name"]).lower()
+
+    if user_input == "exit":
+        break
+    elif user_input == 'help':
+        for i in range(len(commands)):
+            print(commands[i])
+    elif user_input == 'profile':
+        found_password = a_password[2]
+        if found_password in f_password:
+            print(a_lines[2])
+        else:
+            print("Error")
+    #elif user_input == 'achievements':
+
+#while True:
+    #f_lines
+#accounts.close()
+#fish.close()
+
+
 
 
 
