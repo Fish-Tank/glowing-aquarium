1f428c909cc2d3513968838d504be6fcb96984d5
NUK95 <83701192+NUK95@users.noreply.github.com>
2021-05-11 11:56:07 +0200

    changes

--- try_anouk.py
+++ try_anouk.py
@@ -1,10 +1,8 @@
+import pandas as pd
+account_data = pd.read_csv("accounts.txt", delimiter=" ")
+fish_data = pd.read_csv("fish.txt", delimiter=" ")
+
 commands = [
-    "exit > exit the program",
-    "help > show all the commands",
-    "create account > to create a new user",
-    "login > to login to your account",
-    "logout > to logout your account",
-    "fish > enter a fish",
     "profile > show all my profile details ",
     "achievements > show me my cought fish",
     "rating list > enter a fish",
@@ -17,13 +15,36 @@ current_user = {
 
 while True:
         if current_user["name"] == None:
-            user_input = input("Enter a command or type help :>> ").lower()
+            user_input = input("What do you wanna do :>> ").lower()
         else:
             user_input = input("(Welcome: %s) Enter a command or type help :>> " % current_user["name"]).lower()
 
         if user_input == "exit":
             break
+
+        elif user_input == 'achievements':
+            while True:
+                user_name = input("Enter your name to search: ")
+                new_account_data = account_data[['username']]
+                row = new_account_data.to_csv(header=None, index=False).strip('\n').split('\n')
+                if user_name in row:
+                    personal_catches = (fish_data.loc[fish_data['username'] == user_name])
+                    print(personal_catches)
+                    break
+                else:
+                    print("This this username doesn't exist!")
+
         elif user_input == 'profile':
             user_name = input("Enter your name to search: ")
-            personal_catches = (account_data.loc[account_data['username'] == user_name])
-            print(personal_catches)
+            new_account_data = account_data[['username']]
+            row = new_account_data.to_csv(header=None, index=False).strip('\n').split('\n')
+            if user_name in row:
+                personal_catches = (account_data.loc[account_data['username'] == user_name])
+                print(personal_catches)
+                break
+            else:
+                print("This this username doesn't exist!")
+
+        elif user_input == 'rating list':
+           fish_data = fish_data[['type', 'length', 'lake', 'username']]
+           print(fish_data.sort_values('length', ascending=False).reset_index(drop=True))
